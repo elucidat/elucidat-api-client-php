faf7efebb3e03eaba2696cc8a04a378a093b2849
Ian Budden <ian@elucidat.com>
2014-11-04 11:00:20 +0000

    Example of using API for CORS

---
+++ course.php
@@ -0,0 +1,42 @@
+<?php
+
+    function get_from_api ( $endpoint, $public_key, $secret_key )
+    {
+        // configure the request
+        $ch = curl_init();
+        // set the URL
+        curl_setopt($ch, CURLOPT_URL, $endpoint);
+        // set the method
+        $method = "GET";
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // expiry date
+        $expiryDate = gmdate('Y/m/d H:i:s+00:00', strtotime('+15 minutes'));
+        // build the check string
+        $checkString = $endpoint . $method . $expiryDate;
+        // sign it
+        $signature = hash_hmac("sha256", $checkString, $secret_key);
+        // set the headers
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'X-Public: ' . $public_key,
+            'X-Signature: ' . $signature,
+            'X-Expires: ' . $expiryDate
+        ));
+        // make the api call
+        $response = curl_exec($ch);
+        $status = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        // tidy up
+        curl_close($ch);
+        // and send back
+        return array(
+            'status' => $status,
+            'response' => $response
+        );
+    }
+
+    $result = get_from_api('https://api.elucidat.com/releases/{{release_code}}/launch','<YOUR PUBLIC KEY>','<YOUR SECRET KEY>');
+    $outcome = json_decode($result['response'], true);
+
+    print file_get_contents($outcome['url']);
+                
+?>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div id="debug" class="col-md-4"></div>
+
+                <iframe src="course.php"></iframe>
+            </div>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+        <script>
+
+            var Debug_API = function () {};
+            var $debug = $('#debug');
+
+            Debug_API.prototype.debug = function (msg) {
+                $debug.append(msg+'<br />');
+            }
+            // 2004 functions
+            Debug_API.prototype.Initialize = function () { this.debug(' └ Debug_api:Initialize'); return true; };
+            Debug_API.prototype.Terminate = function () { this.debug(' └ Debug_api:Terminate'); return true; };
+            Debug_API.prototype.GetValue = function (nam) { return ''; };
+            Debug_API.prototype.SetValue = function (nam,val) { this.debug(' ┌ Debug_api:SetValue: '+nam+'='+val); return ''; };
+            Debug_API.prototype.Commit = function () { this.debug(' └ Debug_api:Commit'); return true; };
+            Debug_API.prototype.GetLastError = function () { return 0; };
+            Debug_API.prototype.GetErrorString = function (code) { return ""; };
+            Debug_API.prototype.GetDiagnostic = function (code) { return ""; };
+            // 1.2 functions
+            Debug_API.prototype.LMSInitialize = function () { this.debug(' └ Debug_api:LMSInitialize'); return true; };
+            Debug_API.prototype.LMSFinish = function () { this.debug(' └ Debug_api:LMSFinish'); return true; };
+            Debug_API.prototype.LMSGetValue = function (nam) { return ''; };
+            Debug_API.prototype.LMSSetValue = function (nam,val) { this.debug(' ┌ Debug_api:LMSSetValue: '+nam+'='+val); return ''; };
+            Debug_API.prototype.LMSCommit = function () { this.debug(' └ Debug_api:Commit'); return true; };
+            Debug_API.prototype.LMSGetLastError = function () { return 0; };
+            Debug_API.prototype.LMSGetErrorString = function (code) { return ""; };
+            Debug_API.prototype.LMSGetDiagnostic = function (code) { return ""; };
+
+            window['API'] = new Debug_API();
+
+        </script>
+    </body>
+</html>
